c745d3a88ebe4e8cfe20fa5b6eb32d86eb888f2f
ShirlyB-24-tech <160262117+ShirlyB-24-tech@users.noreply.github.com>
2024-02-22 18:29:26 +0100

    adding more js, the apikey doesn't work again

--- src/index.js
+++ src/index.js
@@ -1,20 +1,56 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatedDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 function formatedDate(date) {
   let minutes = date.getminutes();
   let hours = date.gethours();
-  let days = [];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 }
 let day = days[date.getDay()];
+
 if (minutes < 10) {
   minutes = `0${minutes}`;
+
+  return `${day} ${hours}:${minutes}`;
 }
-return `${day} ${hours}: ${minutes}`;
 
 function searchCity(city) {
   let apikey = "96574tb3d9054b0ffad57ffob464d258";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&unit=metric`;
-  axios.get(apiURL).then(updateWeather);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
 }
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Valencia");
